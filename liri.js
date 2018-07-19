d7f060566381b0dbe040426e8fb6f1a990e9d700
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2018-07-19 14:58:31 -0400

    finished set up and coded movie part

--- liri.js
+++ liri.js
@@ -1,13 +1,87 @@
+
+// Required modules and files
 require("dotenv").config();
-
-
+var fs = require("fs");
+var keys = require("./key.js");
+var request = require('request');
+/*var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+//Modular variables
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
+/* general request
+
+request('http://www.google.com', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
 //`my-tweets`
 
+ 
+var client = new Twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+});
+ 
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
  //`spotify-this-song`
+ 
+ 
+ var spotify = new Spotify({
+   id: < your spotify client id>,
+   secret: <your spotify client secret>
+ });
+  
+ spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+   if (err) {
+     return console.log('Error occurred: ' + err);
+   }
+  
+ console.log(data); 
+ });
 
-//`movie-this`
+ 
+ // `movie-this
+  else if (process.argv[2] == "movie"){  */
 
-//`do-what-it-says
+var movieReq = process.argv[3];
+var queryUrl = "http://www.omdbapi.com/?t=" + movieReq + "&y=&plot=short&apikey=trilogy";
+
+console.log(queryUrl);
+
+//if (process.argv[3] !== undefined ) {
+    request(queryUrl, function (error, response, body) {
+    console.log("Title: " + JSON.parse(body).Title);
+    console.log("Year: " + JSON.parse(body).Year);
+    console.log("Movie Rating (IMDB): " + JSON.parse(body).imdbRating);
+    console.log("Movie Rating (Rotten Tomatoes): " + JSON.parse(body).Ratings[1]);
+    console.log ("Country: " + JSON.parse(body).Country);
+    console.log ("Language: " + JSON.parse(body).Language);
+    console.log ("Plot: " + JSON.parse(body).Plot);
+    console.log ("Actors: " + JSON.parse(body).Actors);
+    }); 
+  /*  } else {
+    request("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy", function (error, response, body) {
+        console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+        console.log(JSON.parse(body));
+
+          * Title of the movie.
+       * Year the movie came out.
+       * IMDB Rating of the movie.
+       * Rotten Tomatoes Rating of the movie.
+       * Country where the movie was produced.
+       * Language of the movie.
+       * Plot of the movie.
+       * Actors in the movie
+
+
+    }; */
